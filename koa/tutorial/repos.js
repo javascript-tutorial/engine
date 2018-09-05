24b81df5c2e12de27c25e031db7713665971bace
Ilya Kantor <iliakan@gmail.com>
2018-09-05 10:42:49 +0300

    WIP

---
+++ koa/tutorial/repos.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'iliakan/javascript-tutorial-en': {
+    lang: 'en',
+    githubSecret: '980dgJvrvC'
+  },
+  'xitu/javascript-tutorial-zh': {
+    lang: 'zh',
+    branch: 'zh-hans'
+  },
+  'ockley/javascript-tutorial-da': {
+    lang: 'da'
+  },
+  'SugoiNelson/javascript-tutorial-fr': {
+    lang: 'fr'
+  },
+  'MartinEls/javascript-tutorial-de': {
+    lang: 'de'
+  },
+  'KenjiI/javascript-tutorial-ja': {
+    lang: 'ja'
+  },
+  'iliakan/javascript-tutorial-ru': {
+    lang: 'ru',
+  },
+  'sahinyanlik/javascript-tutorial-tr': {
+    lang: 'tr',
+  },
+  'aruzikulov/javascript-tutorial-uz': {
+    lang: 'uz'
+  }
+};
