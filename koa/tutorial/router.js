f803f4b516722516c036c59e8dcdeb8a8f85aae0
Ilya Kantor <iliakan@gmail.com>
2019-04-15 08:39:41 +0300

    up

--- koa/tutorial/router.js
+++ koa/tutorial/router.js
@@ -10,8 +10,8 @@ let zipview = require('./controller/zipview');
 let router = module.exports = new Router();
 
 router.get('/tutorial/reload', reload.get);
+router.get('/tutorial/tree', tree.get);
 router.get('/task/:slug', task.get);
-router.get('/tree', tree.get);
 router.get('/tutorial/zipview/:name', zipview.get);
 
 router.get('/:slug', article.get);
