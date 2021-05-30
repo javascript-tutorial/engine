6ee19cae82228137239c03d338428093d3af6d13
Ilya Kantor <iliakan@gmail.com>
2021-05-30 17:57:11 +0300

    minor fixes

--- livereloadServer.js
+++ livereloadServer.js
@@ -1,11 +1,23 @@
 let tinyLr = require('tiny-lr');
 let debounce = require('lodash/debounce');
+let config = require('config');
+let path = require('path');
+let fs = require('fs');
 
 class LivereloadServer {
   constructor() {
-    this.tinyLrServer = new tinyLr.Server({
-      livereload: require.resolve('livereload-js/dist/livereload.js')
-    });
+    let options = {
+      livereload: require.resolve('livereload-js/dist/livereload.js'),
+    };
+
+    if (process.env.HTTPS) {
+      Object.assign(options, {
+        key: fs.readFileSync(path.join(config.certDir, 'local-key.pem')),
+        cert: fs.readFileSync(path.join(config.certDir, 'local.pem')),
+      });
+    }
+
+    this.tinyLrServer = new tinyLr.Server(options);
 
     this.tinyLrServer.listen(35729, undefined, () => {
       console.log("Livereload server listening");
