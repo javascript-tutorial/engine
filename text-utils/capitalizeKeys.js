e61ac7bc8b0d2879f84d7f6ed9a1ebcb62700d0c
Ilya Kantor <iliakan@users.noreply.github.com>
2024-11-05 13:33:49 +0100

    minor fixes

--- text-utils/capitalizeKeys.js
+++ text-utils/capitalizeKeys.js
@@ -1,22 +1,29 @@
 
-function capitalizeKeys(obj, mapper) {
-
+function capitalizeKeys(obj, mapper, seen = new WeakSet()) {
   if (Array.isArray(obj)) {
-    let arr = obj;
-    return arr.map(item => capitalizeKeys(item, mapper));
+    return obj.map(item => capitalizeKeys(item, mapper, seen));
   }
 
-  if (Object.prototype.toString.apply(obj) !== '[object Object]') {
+  if (Object.prototype.toString.call(obj) !== '[object Object]') {
     return obj;
   }
+
+  if (seen.has(obj)) {
+    throw new Error("Circular reference detected");
+  }
+
+  seen.add(obj); // Mark the object as visited.
 
   let output = {};
 
   for (let key in obj) {
     let keyCapitalized = key.replace(/_(\w)/g, (match, letter) => letter.toUpperCase());
     if (mapper) keyCapitalized = mapper(keyCapitalized);
-    output[keyCapitalized] = capitalizeKeys(obj[key], mapper);
+    output[keyCapitalized] = capitalizeKeys(obj[key], mapper, seen);
   }
+
+  seen.delete(obj); // Remove from `seen` after processing to allow for reuse in non-circular parts.
+
   return output;
 }
 
